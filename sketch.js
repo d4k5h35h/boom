497ce33a934a71b164897d7aa6f70ea241b5182a
DAKSHESH CHITTOORI <64137511+d4k5h35h@users.noreply.github.com>
2020-06-09 17:40:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var song;
+var button;
+var amp;
+let bgColor = 0;
+
+function setup() {
+  createCanvas(400, 400);
+  bgColor = random(255);
+  song = loadSound("d.mp3", loaded);
+  amp = new p5.Amplitude();
+
+}
+
+function loaded() {
+  button = createButton("play");
+  button.mousePressed(togglePlaying);
+}
+
+function togglePlaying() {
+  if (!song.isPlaying()) {
+    song.play();
+    song.setVolume(1);
+    button.html("pause");
+  } else {
+    song.pause();
+    button.html("play");
+  }
+}
+
+function draw() {
+  background(bgColor);
+  var vol = amp.getLevel();
+  var diam = map(vol, 0, 0.3, 10, 225);
+  push();
+  fill(205, 209, 209);
+  stroke(51);
+  strokeWeight(4);
+  ellipse(200, 200, diam, diam);
+  pop();
+  push();
+  fill(0);
+  ellipse(200, 200, diam / 3.5, diam / 3.5);
+  pop();
+  push();
+  noFill();
+  stroke(255);
+  strokeWeight(37.5);
+  rect(0, 0, 400, 400, 50);
+  pop();
+  changer();
+}
+
+function changer() {
+  if (frameCount % 10 === 0 && song.isPlaying()) {
+    bgColor = color(random(255), random(255), random(255));
+  } else if(!song.isPlaying()){
+    bgColor = 0;
+  }
+}
